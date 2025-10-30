b3c7a94817af62e9b16d2042070315cb25e5faca
Patrick Solamillo <patricksolamillo@proton.me>
2025-10-30 10:35:52 -0400

    add multiplication table logic when button is clicked

--- app/src/main/java/com/example/midterm_patrick_solalmillo/MainActivity.java
+++ app/src/main/java/com/example/midterm_patrick_solalmillo/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.midterm_patrick_solalmillo;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +13,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText multNumber;
+    private Button multButton;
+    private ListView multResult;
+
+    EditText editText;
+    int product;
+    private ArrayAdapter<String> adapter;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +35,29 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        editText = (EditText)findViewById(R.id.inputNum);
+        multButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        if(v.getId() == R.id.buttonM){
+            StringBuffer buffer = new StringBuffer();
+
+            String fs = editText.getText().toString();
+
+            int numMult = Integer.parseInt(fs);
+
+            for (int i = 1; i <= 10; i++) {
+                product = (i * numMult);
+                buffer.append(numMult + " X " +
+                        i + " = " + numMult + "\n\n");
+            }
+
+        }
+
+
     }
 }
